aab995d41bba39b5f4289c5b9365f5a2051d71c4
Veerendra <pvnreddy1997@gmail.com>
2023-09-29 22:06:53 +0530

    Add initial react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles.css'
+
+function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  )
+}
+
+export default App
